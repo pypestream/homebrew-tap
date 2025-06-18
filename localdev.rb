e9764d346f36d617f26ffc5c9c4de7a25b46bff7
sqaisar <95719467+sqaisar@users.noreply.github.com>
2025-06-18 06:19:37 +0000

    Update formula to latest release and release assets

--- localdev.rb
+++ localdev.rb
@@ -1,22 +1,25 @@
 class Localdev < Formula
   desc "Local development tool for Kubernetes environments"
   homepage "https://github.com/pypestream/homebrew-tap"
-  version "v1.0.7"
+  version "v1.0.8"
 
   if Hardware::CPU.arm?
-    url "https://github.com/pypestream/homebrew-tap/releases/download/v1.0.7/localdev-darwin-arm64"
-    sha256 "77b48c903ae97f28376f247a223038ee723fceeb7f7b893787956559635971c4"
+    url "https://github.com/pypestream/homebrew-tap/releases/download/v1.0.8/localdev-darwin-arm64"
+    sha256 "33d723eb3b02f72b42f1e2ff6962ae80747be458d8fa1b0a9c0ea3952c705973"
   else
-    url "https://github.com/pypestream/homebrew-tap/releases/download/v1.0.7/localdev-darwin-amd64"
-    sha256 "f0493938766e4ca3dd216d1eb12e8cf4bc6920f31bf4da3071a47b7c98b80bb4"
+    url "https://github.com/pypestream/homebrew-tap/releases/download/v1.0.8/localdev-darwin-amd64"
+    sha256 "876373c7f06e078402d30da218b8ae03b0d32115384923807e11a2b79683963b"
   end
 
   def install
-    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    bin.install "localdev-darwin-\#{arch}" => "localdev"
+    if Hardware::CPU.arm?
+      bin.install "localdev-darwin-arm64" => "localdev"
+    else
+      bin.install "localdev-darwin-amd64" => "localdev"
+    end
   end
 
   test do
-    system "\#{bin}/localdev", "--version"
+    system "#{bin}/localdev", "--version"
   end
 end
