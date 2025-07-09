7acc4ee16216f6395edb894d7b5a436501297375
sqaisar <95719467+sqaisar@users.noreply.github.com>
2025-07-09 16:20:44 +0000

    Update formula to latest release and release assets

--- localdev.rb
+++ localdev.rb
@@ -1,14 +1,14 @@
 class Localdev < Formula
   desc "Local development tool for Kubernetes environments"
   homepage "https://github.com/pypestream/homebrew-tap"
-  version "v1.0.9"
+  version "v1.0.10"
 
   if Hardware::CPU.arm?
-    url "https://fs.gcp.pype.tech/releases/download/v1.0.9/localdev-darwin-arm64"
-    sha256 "71eb197f07302565ff72978445b286cfdaef000086a9adf5db1efe4fc002ee8f"
+    url "https://fs.gcp.pype.tech/releases/download/v1.0.10/localdev-darwin-arm64"
+    sha256 "871926d90c6d3c882f0cd79a6f026069133ebb486c28bf99e7240ac05bc46e28"
   else
-    url "https://fs.gcp.pype.tech/releases/download/v1.0.9/localdev-darwin-amd64"
-    sha256 "6b3e70bc0b262b84a2106a0a24cac4b52531f8f5d036cf1292897765a0b08712"
+    url "https://fs.gcp.pype.tech/releases/download/v1.0.10/localdev-darwin-amd64"
+    sha256 "7377151a677cf8d7dfed1309b93455a97c329177d237a2d896470f0f41acd27b"
   end
 
   def install
